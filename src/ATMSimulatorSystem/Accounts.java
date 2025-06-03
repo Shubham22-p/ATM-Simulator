b83280c2af764c9c971d81f52d2377825a654474
Shubham Pingale <sdpingale22@gmail.com>
2025-06-03 12:22:51 +0530

    This method checks if a user's account exists, takes input for account details, inserts them into the database using a secure prepared statement, and returns the newly generated account number if successful.

--- src/ATMSimulatorSystem/Accounts.java
+++ src/ATMSimulatorSystem/Accounts.java
@@ -2,15 +2,50 @@ package ATMSimulatorSystem;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Accounts {
 
-    private Connection connection;
+    private Connection connections;
     private Scanner scanner;
     public Accounts(Connection connection, Scanner scanner){
-        this.connection = connection;
+        this.connections = connection;
         this.scanner = scanner;
 
     }
+    public long open_account(String email){
+        if(!account_exist(email)) {
+            String open_account_query = "INSERT INTO Accounts(account_number, full_name, email, balance, security_pin) VALUES(?, ?, ?, ?, ?)";
+            scanner.nextLine();
+            System.out.print("Enter Full Name: ");
+            String full_name = scanner.nextLine();
+            System.out.print("Enter Initial Amount: ");
+            double balance = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.print("Enter Security Pin: ");
+            String security_pin = scanner.nextLine();
+            try {
+                long account_number = generateAccountNumber();
+                PreparedStatement preparedStatement = connection.prepareStatement(open_account_query);
+                preparedStatement.setLong(1, account_number);
+                preparedStatement.setString(2, full_name);
+                preparedStatement.setString(3, email);
+                preparedStatement.setDouble(4, balance);
+                preparedStatement.setString(5, security_pin);
+                int rowsAffected = preparedStatement.executeUpdate();
+                if (rowsAffected > 0) {
+                    return account_number;
+                } else {
+                    throw new RuntimeException("Account Creation failed!!");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        throw new RuntimeException("Account Already Exist");
+
+    }
+
 }
