3586d2463272ef8b55f21510d4f22a2020d824b3
Shubham Pingale <sdpingale22@gmail.com>
2025-06-03 12:17:36 +0530

    Created Accounts Class and in that class i have declared private variables

--- src/ATMSimulatorSystem/Accounts.java
+++ src/ATMSimulatorSystem/Accounts.java
@@ -1,10 +1,16 @@
 package ATMSimulatorSystem;
 
-import java.sql.*;
+
+import java.sql.Connection;
 import java.util.Scanner;
 
 public class Accounts {
-private Connection connection;
-private Scanner scanner ;
 
+    private Connection connection;
+    private Scanner scanner;
+    public Accounts(Connection connection, Scanner scanner){
+        this.connection = connection;
+        this.scanner = scanner;
+
+    }
 }
